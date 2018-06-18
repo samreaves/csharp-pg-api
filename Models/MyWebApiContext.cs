52abbe0259202480036882f569b38dd34154b4c8
Sam Reaves <sreaves@kabbage.com>
2018-06-18 18:39:13 -0400

    Initial commit of C# API using PostgreSQL and Entity Framework

---
+++ Models/MyWebApiContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApi.Models
+{
+    public class MyWebApiContext : DbContext
+    {
+        public MyWebApiContext(DbContextOptions<MyWebApiContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
